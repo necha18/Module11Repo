f3ba6822dda4471e5edb5d92fc91ec7c953f560d
necha18 <s3844216@students.fscj.edu>
2026-08-04 10:59:11 -0400

    Update author information and add welcome message

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-# Firstname Lastname
-# 11/15/2024
+# Neisha Charles
+# 08/04/2026
 # A simple Python program to print a message
 
+# Neisha Charles
+# 08/04/2026
+# added new message
+
 print("Hello, World!")
+
+print("Welcome to Github!")
